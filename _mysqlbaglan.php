93689152d00c1b4f56b27b6513fec1f8f31a0cac
deryaozturkk <72493701+deryaozturkk@users.noreply.github.com>
2022-06-22 23:25:23 +0300

    Add files via upload

---
+++ _mysqlbaglan.php
@@ -0,0 +1,24 @@
+<?php 
+   $server = 'sql212.epizy.com'; 
+   
+   $user = 'epiz_32005398'; 
+   
+   $password = '6Ki0Ceeat1D'; 
+   
+   $database = 'epiz_32005398_sifreligiris'; 
+   
+   $baglanti = mysqli_connect($server,$user,$password,$database); 
+   
+    
+   
+   if (!$baglanti) { 
+   
+       echo "MySQL sunucu ile baglanti kurulamadi! </br>"; 
+   
+       echo "HATA: " . mysqli_connect_error(); 
+   
+       exit; 
+   
+   } 
+   
+   ?>
