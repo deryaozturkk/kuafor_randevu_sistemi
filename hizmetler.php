9de5aeae781131a4cefebe8f3ea78d1ea06a6bb8
deryaozturkk <72493701+deryaozturkk@users.noreply.github.com>
2022-06-22 23:32:26 +0300

    Add files via upload

---
+++ hizmetler.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Hizmetlerimiz</title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initialscale=1">
+<link rel="stylesheet"
+href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootst
+rap.min.css">
+<script
+src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.m
+in.js"></script>
+<script
+src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstra
+p.min.js"></script>
+</head>
+<body>
+<div class="container">
+<div class="page-header">
+<div class="container-fluid">
+
+<h2><b><span style="background:#ed6f6f">
+Hizmetlerimiz</span></b></h2>     
+  </div>           
+</div>
+<h4><b>Lütfen almak istediğiniz hizmeti seçiniz:</b></h4>
+<ul class="nav nav-pills nav-stacked">
+<li><a href="randevusac.php">Saç Hizmetleri</a></li>
+<li><a href="randevutirnak.php">Tırnak Hizmetleri</a></li>
+<li><a href="randevumakeup.php">Makyaj Hizmetleri</a></li>
+<li><a href="randevuciltbakimi.php">Cilt Bakımı Hizmetleri</a></li>
+</ul>
+
+<div class="container">
+  <h4>Çalışma Saatlerimiz:</h4>
+  <table class="table">
+    <thead>
+      <tr>
+        <th>Haftaiçi</th>
+        <th>Haftasonu</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>10.00-22.00</td>
+        <td>10.00-21.00</td>
+        
+      </tr>
+  
+      
+    </tbody>
+  </table>
+</div>
+
+</body>
+</html>
+
+
+
+
+</body>
+</html>
