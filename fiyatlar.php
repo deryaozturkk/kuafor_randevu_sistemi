a668cd0fa7f14587fea9bc2bdabe9d6cd27928e3
deryaozturkk <72493701+deryaozturkk@users.noreply.github.com>
2022-06-22 23:30:13 +0300

    Add files via upload

---
+++ fiyatlar.php
@@ -0,0 +1,139 @@
+<!DOCTYPE html>
+<html lang="tr">
+<head>
+  <title>Fiyatlarımız</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" 
+  href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+<div class="container mt-4">
+<h2>Fiyat Listemiz</h2>
+<table class="table">
+    <thead>
+<tr>
+    <th>Saç Hizmetleri</th>
+    <th>Fiyatlarımız</th>
+</tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>Saç Kesimi</td>
+            <td>100 TL</td>             
+        </tr>
+       
+        <tr>
+            <td>Fön</td>
+            <td>70 TL</td>
+        </tr>
+        <tr>
+            <td>Keratin Bakım</td>
+            <td>300 TL</td>
+        </tr>
+
+        <tr>
+            <td>Saç Boyama</td>
+            <td>450 TL</td>
+        </tr>
+            <td>Maşa</td>
+            <td>70 TL</td>
+        <tr>
+            <td>Topuz</td>
+            <td>100 TL</td>
+        </tr>
+    </tbody>
+</table>
+<table  class="table">
+    <thead>
+        <tr>
+            <th>Tırnak Hizmetleri</th>
+            <th>Fiyatlarımız</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>Nail Art</td>
+            <td>20-200 TL</td>
+        </tr>
+        <tr>
+            <td>Manikür</td>
+            <td>130 TL</td>
+        </tr>
+        <tr>
+            <td>Pedikür</td>
+            <td>150 TL</td>
+        </tr>
+        <tr>
+            <td>Kalıcı Oje</td>
+            <td>175 TL</td>
+        </tr>
+        <tr>
+            <td>Protez Tırnak</td>
+            <td>450 TL</td>
+        </tr>
+        <tr>
+            <td>Jel Tırnak</td>
+            <td>400 TL</td>
+        </tr>
+    </tbody>
+
+</table>
+<table  class="table">
+    <thead>
+        <tr>
+            <th>Makyaj Hizmetleri</th>
+            <th>Fiyatlarımız</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>Gündelik Makyaj</td>
+            <td>300 TL</td>
+        </tr>
+        <tr>
+            <td>İpek Kirpik</td>
+            <td>400 TL</td>
+        </tr>
+        <tr>
+            <td>Gece Makyajı</td>
+            <td>400 TL</td>
+        </tr>
+        <tr>
+            <td>Kirpik Lifting</td>
+            <td>500 TL</td>
+        </tr>
+        <tr>
+            <td>Mikroblading</td>
+            <td>1350 TL</td>
+        </tr>
+        <tr>
+            <td>Kaş Laminasyonu</td>
+            <td>500 TL</td>
+        </tr>
+    </tbody>
+    <table  class="table">
+    <thead>
+        <tr>
+            <th>Cilt Bakımı Hizmetleri</th>
+            <th>Fiyatlarımız</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>Cilt Bakımı</td>
+            <td>500 TL</td>
+        </tr>
+        <tr>
+            <td>Spa</td>
+            <td>700 TL</td>
+        </tr>
+    </tbody>
+</table>
+
+</div>
+</body>
+</html>
