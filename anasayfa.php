e6c759fdcd0eed905d9a98cf278e248ec1819338
deryaozturkk <72493701+deryaozturkk@users.noreply.github.com>
2022-06-22 23:28:08 +0300

    Add files via upload

---
+++ anasayfa.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Anasayfa</title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initialscale=1">
+<link rel="stylesheet"
+href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootst
+rap.min.css">
+<script
+src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.m
+in.js"></script>
+<script
+src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstra
+p.min.js"></script>
+</head>
+<body>
+
+<br>
+<br>
+<nav class="navbar navbar-inverse">
+    <div class="container-fluid">
+        <ul class="nav navbar-nav">
+            <li class="active"><a href="#">Ana Sayfa</a></li>
+            <li class="nav-warning"><a href="hizmetler.php">Randevu</a></li>
+            <li><a href="fiyatlar.php">Fiyatlar</a></li>
+            <li><a href="https://github.com/deryaozturkk">Github</a></li>
+            <li><a href="cikis.php">Çıkış Yap</a></li>
+        </ul>
+    </div>
+</nav>
+
+
+<div class="container">
+<h2>Bir Randevu Kadar Yakındayız</h2>  
+  <div id="dongu" class="carousel slide" data-ride="carousel">
+    <!-- Indicators -->
+    <ol class="carousel-indicators">
+      <li data-target="#" data-slide-to="0" class="active"></li>
+      <li data-target="#" data-slide-to="1"></li>
+      <li data-target="#" data-slide-to="2"></li>
+    </ol>
+
+    <!-- Wrapper for slides -->
+    <div class="carousel-inner">
+    <div class="item active">
+        <img src="https://www.falconinterior.com/wp-content/uploads/2019/07/10000.jpg" alt="first photo" style="width:50%;">
+      </div>
+
+      <div class="item">
+        <img src="https://www.falconinterior.com/wp-content/uploads/2019/07/40000.jpg" alt="second photo" style="width:50%;">
+      </div>
+
+      <div class="item">
+        <img src="https://www.falconinterior.com/wp-content/uploads/2019/07/20000.jpg" alt="third photo" style="width:50%;">
+      </div>
+    </div>
+
+    <!-- Left and right controls -->
+    <a class="left carousel-control" href="#dongu" data-slide="prev">
+      <span class="glyphicon glyphicon-chevron-left"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+    <a class="right carousel-control" href="#dongu"  data-slide="next">
+      <span class="glyphicon glyphicon-chevron-right"></span>
+      <span class="sr-only">Next</span>
+    </a>
+  </div>
+</div>
+
+<center>
+<h2 text-align="right">Derya Beauty Center Nerelerde Var?</h2>
+<br>
+
+<div class="btn-group btn-group-justified">
+  <a href="#" class="btn btn-info">İSTANBUL</a>
+  <a href="#" class="btn btn-info">ANKARA</a>
+  <a href="#" class="btn btn-info">BURSA</a>
+  <a href="#" class="btn btn-info">İZMİR</a>
+  <a href="#" class="btn btn-info">AYDIN</a>
+  <a href="#" class="btn btn-info">ESKİŞEHİR</a>
+  <a href="#" class="btn btn-info">BOLU</a>
+</div>
+<br>
+<br>
+
+<div class="well well-sm">
+<p align="center">Derya Beauty Center A.Ş.
+<br> Her Hakkı Saklıdır. Copyright &copy 2022</p>
+</div>
+
+
+</div>
+</body>
+</html>
